d50fd1cfc8080a696bd6136d9d2df0279539be48
Brunno (brunno-pontes) <brunnovivocq@gmail.com>
2024-11-28 15:42:01 -0300

    atulizado

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-window.alert('Estudndo JavaScript')
-window.confirm('Estudando JavaScript')
+
+let nome = window.prompt('Qual seu nome?')
+document.write(`Seja bem vindo!<br> <strong>${nome}</strong>, seu nome tem ${nome.length} letras.`)
+let n1 = Number(window.prompt('Digite um numero'))
+let n2 = Number(window.prompt('Digite outro numero')) 
+let res = n1 + n2
+document.write(`<p>A soma entre ${n1} e ${n2} e igual a ${res}</p>`).upperCase()
